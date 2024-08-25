db9f310c7c78dbbccb8b96dde91cb0e52abfaaf6
SchelZ <67759165+SchelZ@users.noreply.github.com>
2024-08-26 02:41:40 +0300

    Create ble.c

---
+++ ble.c
@@ -0,0 +1,44 @@
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEScan.h>
+
+#define scanTime 5
+
+class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks {
+  void onResult(BLEAdvertisedDevice advertisedDevice) {
+    
+    Serial.printf("Advertised Device: %s \n", advertisedDevice.toString().c_str());   // Print the device's address / name
+
+    if (advertisedDevice.haveManufacturerData()) {
+      String manufacturerData = advertisedDevice.getManufacturerData();
+      Serial.print("Received Message: ");
+      const char* data = manufacturerData.c_str();
+      for (size_t i = 2; i < manufacturerData.length(); i++) { Serial.printf("%c", (char)data[i]);  }  // Print the manufacture data ( message ) 
+      Serial.println();
+    }
+  }
+};
+
+void setup() {
+  Serial.begin(115200);
+
+  // Initialize BLE
+  BLEDevice::init("");
+  
+  BLEScan* pBLEScan = BLEDevice::getScan();
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+
+  // Set the scan interval and window
+  pBLEScan->setInterval(100);
+  pBLEScan->setWindow(99);
+
+  // Start the scan
+  pBLEScan->setActiveScan(true);     // Active scan uses more power but returns more data
+  pBLEScan->start(scanTime, false);  // The second argument is to not continue scanning after the timeout
+}
+
+void loop() {
+  // Start scanning again after previous scan ends
+  BLEScan* pBLEScan = BLEDevice::getScan();
+  pBLEScan->start(scanTime, false);
+}
